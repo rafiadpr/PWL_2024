9e632ce8eae76b8f4ce183fd16671175ba729d2a
rafiadpr <work.rafiadrianp@gmail.com>
2026-02-24 00:42:57 +0700

    [update] : finished js2 no 3 : controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about()
+    {
+        return 'Nama: Rafi Adrian <br> NIM: 244107020026';
+    }
+}
