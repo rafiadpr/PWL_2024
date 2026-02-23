53f34d9609458ed77f5f93036d8d75512bdd2d9c
rafiadpr <work.rafiadrianp@gmail.com>
2026-02-24 00:12:31 +0700

    [update] : finished js2 no 2 : routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+Route::get('mahasiswa', function ($id) {
+});
+Route::post('mahasiswa', function ($id) {
+});
+Route::put('mahasiswa', function ($id) {
+});
+Route::delete('mahasiswa', function ($id) {
+});
+Route::get('mahasiswa/{id}', function ($id) {
+});
+Route::put('mahasiswa/{id}', function ($id) {
+});
+Route::delete('mahasiswa/{id}', function ($id) {
+});
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+Route::get('/world', function () {
+    return 'World';
+});
+Route::get('/about', function () {
+    return 'Rafi Adrian Prasetya / 244107020026';
+});
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+Route::get('/articles/{id}', function ($articleId) {
+    return 'Halaman Artikel dengan ID-' . $articleId;
+});
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
